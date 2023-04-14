3ada77f3582394a09e90decdfc16170aa4c24a4e
Fahriza Ramadhani Putra <fahrizarp01@gmail.com>
2023-04-14 08:32:08 +0700

    14-4-2023
    Repair css admin

--- application/controllers/Login.php
+++ application/controllers/Login.php
@@ -1,38 +1,44 @@
-<?php defined('BASEPATH') OR exit('No direct script access allowed');
+<?php defined('BASEPATH') or exit('No direct script access allowed');
 
-class Login extends CI_Controller {
+class Login extends CI_Controller
+{
 
-	function __construct(){
+	function __construct()
+	{
 		parent::__construct();
 		//$this->load->model('Auth_model');
 	}
-		
-	public function index() {
+
+	public function index()
+	{
 		$this->load->view('admin/login');
 	}
 
-	function getlogin() {
+	function getlogin()
+	{
 		$user = $this->input->post('username');
 		$pass = $this->input->post('password');
-		$user = $this->auth->log_admin($user,$pass);
-			if($user==true){
-				// $this->session->set_userdata($user);
-				$this->session->set_userdata('id_admin',$user->id_admin);
-				$this->session->set_userdata('admin_username',$user->admin_username);
-				$this->session->set_userdata('admin_password',$user->admin_password);
-				$this->session->set_userdata('admin_view_password',$user->admin_view_password);
-				$this->session->set_userdata('admin_level',$user->admin_level);
-				$data['hasil'] = 1;
-				echo json_encode($data);
-			}else{
-				$data['hasil'] = 0;
-				echo json_encode($data);
-			}
+		$user = $this->auth->log_admin($user, $pass);
+		if ($user == true) {
+			// $this->session->set_userdata($user);
+			$this->session->set_userdata('id_admin', $user->id_admin);
+			$this->session->set_userdata('admin_nama', $user->admin_nama);
+			$this->session->set_userdata('admin_username', $user->admin_username);
+			$this->session->set_userdata('admin_password', $user->admin_password);
+			$this->session->set_userdata('admin_view_password', $user->admin_view_password);
+			$this->session->set_userdata('admin_level', $user->admin_level);
+			$data['hasil'] = 1;
+			echo json_encode($data);
+		} else {
+			$data['hasil'] = 0;
+			echo json_encode($data);
+		}
 	}
 
-	function logout(){
+	function logout()
+	{
 		//helper_log("logout", "Logout");
 		$this->session->sess_destroy();
-		redirect('Login','refresh');
-    }
+		redirect('Login', 'refresh');
+	}
 }
